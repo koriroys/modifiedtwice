a210878af408c075cffe33989273e1b738a84d1b
Kori Roys <kori@koriroys.com>
2015-08-12 16:26:57 +0200

    books mock

---
+++ server/mocks/books.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var express = require('express');
+  var booksRouter = express.Router();
+
+  booksRouter.get('/', function(req, res) {
+    res.send({
+      'books': []
+    });
+  });
+
+  booksRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  booksRouter.get('/:id', function(req, res) {
+    res.send({
+      'books': {
+        id: req.params.id
+      }
+    });
+  });
+
+  booksRouter.put('/:id', function(req, res) {
+    res.send({
+      'books': {
+        id: req.params.id
+      }
+    });
+  });
+
+  booksRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/books', booksRouter);
+};
